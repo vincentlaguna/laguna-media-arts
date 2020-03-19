33d3c2f0793fcb2376ca48f17092ad7ebf9aec42
vincentlaguna <vincentlaguna@live.com>
2020-03-19 04:11:17 +0000

    added link to other project

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def hello
-    render html: "Future Photography works of Vincent Laguna"
+    render html: "Future Photography works of Vincent Laguna._____Checkout my collaborative project Mission Share Kindness: https://radiant-river-10468.herokuapp.com/"
   end
 end
